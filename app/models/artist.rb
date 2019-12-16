53150fdde98282753a7e74517749aba915582e04
Mariola Pasko <mariolapasko@Mariolas-MacBook-Air.local>
2019-12-16 15:37:21 +0000

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,9 +2,43 @@ class Artist
 
   attr_reader :name, :years_experience
 
+  @@all = []
+
   def initialize(name, years_experience)
     @name = name
     @years_experience = years_experience
+    @@all << self
   end
 
+  def self.all
+    @@all
+  end 
+
+  #Returns an `array` all the paintings by an artist
+  def paintings
+    Painting.all.select {|painting| painting.artist == self}
+  end 
+
+  def galleries #Returns an `array` of all the galleries that an artist has paintings in
+    paintings.map {|painting| painting.gallery}
+  end 
+
+  def cities #Return an `array` of all cities that an artist has paintings in
+    galleries.map {|gallery| gallery.city}
+  end 
+
+  def self.total_experience #Returns an `integer` that is the total years of experience of all artists
+     self.all.map {|artist| artist.years_experience}.reduce(:+) 
+  end 
+
+  def self.most_prolific #Returns an `instance` of the artist with the highest amount of paintings per year of experience.
+    self.all.max_by {|a| a.years_experience / a.paintings.count}
+  end 
+
+  def create_painting(title, price, gallery) #Given the arguments of `title`, `price` and `gallery`, creates a new painting belonging to that artist
+    Painting.new(title, price, self, gallery)
+  end 
+
 end
+
+
--- tools/console.rb
+++ tools/console.rb
@@ -1,7 +1,13 @@
 require_relative '../config/environment.rb'
 
+a1 = Artist.new("Ben", 10)
+a2 = Artist.new("Bob", 4)
 
+gal1 = Gallery.new("Gallery1", "London")
+gal2 = Gallery.new("Gallery2", "Paris")
 
+paint1 = Painting.new("Title", 100, a1, gal1)
+paint2 = Painting.new("Title2", 200, a2, gal2)
 
 binding.pry
 
